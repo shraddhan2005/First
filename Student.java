cba50029490da639ca43ed435360d34bcd39258b
shraddhan2005 <nayakshraddha320@gmail.com>
2025-04-29 09:57:51 +0530

    Save my file to main branch

--- Student.java
+++ Student.java
@@ -14,19 +14,12 @@ class Study{
             System.out.println("dob:"+dob);
         }
     }
-        class Course{
-            void show(){
-        System.out.println("maths:021");
-        System.out.println("BDA:022");
-        System.out.println("Devops:023");
-    }
-}
+
 public class Student{
     public static void main(String[] args){
         Study s1=new Study ("alice",19,"20-07-2005");
-        Course c1=new Course();
         s1.display();
-        c1.show();
+        
     }
 
 }
